7ac4956c25b3f51474d0731d2378d86b79038869
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-17 01:09:55 +0530

    todo remove added

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -2,14 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       inputGoals: "",
-      goals: []
-    }
+      goals: [],
+    };
   },
   methods: {
     addGoal() {
       this.goals.push(this.inputGoals);
-    }
-  }
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    },
+  },
 });
 
 app.mount("#user-goals");

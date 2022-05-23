7d1394534ae8b0eeb0b7b25c67ffa4b3aa38d307
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-23 10:40:02 +0530

    first app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        name: "Nikhil singh",
+        phone: "9453511110",
+        email: "niksingh@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
